2861dbd7008629f57573eb14cc848106618378ea
Miguel <migueortiz0125@gmail.com>
2023-12-08 08:44:18 -0800

    second commit

---
+++ .next/server/chunks/819.js
@@ -0,0 +1,163 @@
+"use strict";
+exports.id = 819;
+exports.ids = [819];
+exports.modules = {
+
+/***/ 2854:
+/***/ ((module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.a(module, async (__webpack_handle_async_dependencies__, __webpack_async_result__) => { try {
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   Z: () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(5893);
+/* harmony import */ var framer_motion__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(6197);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(6689);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_2__);
+var __webpack_async_dependencies__ = __webpack_handle_async_dependencies__([framer_motion__WEBPACK_IMPORTED_MODULE_1__]);
+framer_motion__WEBPACK_IMPORTED_MODULE_1__ = (__webpack_async_dependencies__.then ? (await __webpack_async_dependencies__)() : __webpack_async_dependencies__)[0];
+
+
+
+const quote = {
+    hidden: {
+        opacity: 1
+    },
+    visible: {
+        opacity: 1,
+        transition: {
+            delay: 0.5,
+            staggerChildren: 0.08
+        }
+    }
+};
+const singleWord = {
+    hidden: {
+        opacity: 0,
+        y: 50
+    },
+    visible: {
+        opacity: 1,
+        y: 0,
+        transition: {
+            duration: 1
+        }
+    }
+};
+const AnimatedText = ({ text, className = "" })=>{
+    return /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("div", {
+        className: "py-2 w-full mx-auto flex flex-col items-center justify-center  text-center     overflow-hidden sm:py-0",
+        children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(framer_motion__WEBPACK_IMPORTED_MODULE_1__.motion.h1, {
+            className: `inline-block text-dark dark:text-light
+      text-8xl font-bold w-full capitalize  ${className} xl:text-6xl`,
+            variants: quote,
+            initial: "hidden",
+            animate: "visible",
+            children: text.split(" ").map((char, index)=>{
+                return /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)(framer_motion__WEBPACK_IMPORTED_MODULE_1__.motion.span, {
+                    className: "inline-block",
+                    variants: singleWord,
+                    children: [
+                        char,
+                        "\xa0"
+                    ]
+                }, char + "-" + index);
+            })
+        })
+    });
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (AnimatedText);
+
+__webpack_async_result__();
+} catch(e) { __webpack_async_result__(e); } });
+
+/***/ }),
+
+/***/ 6934:
+/***/ ((module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.a(module, async (__webpack_handle_async_dependencies__, __webpack_async_result__) => { try {
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   Z: () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(5893);
+/* harmony import */ var framer_motion__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(6197);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(6689);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_2__);
+var __webpack_async_dependencies__ = __webpack_handle_async_dependencies__([framer_motion__WEBPACK_IMPORTED_MODULE_1__]);
+framer_motion__WEBPACK_IMPORTED_MODULE_1__ = (__webpack_async_dependencies__.then ? (await __webpack_async_dependencies__)() : __webpack_async_dependencies__)[0];
+
+
+
+const TransitionEffect = ()=>{
+    return /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.Fragment, {
+        children: [
+            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(framer_motion__WEBPACK_IMPORTED_MODULE_1__.motion.div, {
+                className: "fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-primary",
+                initial: {
+                    x: "100%",
+                    width: "100%"
+                },
+                animate: {
+                    x: "0%",
+                    width: "0%"
+                },
+                exit: {
+                    x: [
+                        "0%",
+                        "100%"
+                    ],
+                    width: [
+                        "0%",
+                        "100%"
+                    ]
+                },
+                transition: {
+                    duration: 0.8,
+                    ease: "easeInOut"
+                }
+            }),
+            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(framer_motion__WEBPACK_IMPORTED_MODULE_1__.motion.div, {
+                className: "fixed top-0 bottom-0 right-full w-screen h-screen z-20 bg-light dark:bg-dark",
+                initial: {
+                    x: "100%",
+                    width: "100%"
+                },
+                animate: {
+                    x: "0%",
+                    width: "0%"
+                },
+                transition: {
+                    delay: 0.2,
+                    duration: 0.8,
+                    ease: "easeInOut"
+                }
+            }),
+            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(framer_motion__WEBPACK_IMPORTED_MODULE_1__.motion.div, {
+                className: "fixed top-0 bottom-0 right-full w-screen h-screen z-10 bg-dark dark:bg-light",
+                initial: {
+                    x: "100%",
+                    width: "100%"
+                },
+                animate: {
+                    x: "0%",
+                    width: "0%"
+                },
+                transition: {
+                    delay: 0.4,
+                    duration: 0.8,
+                    ease: "easeInOut"
+                }
+            })
+        ]
+    });
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (TransitionEffect);
+
+__webpack_async_result__();
+} catch(e) { __webpack_async_result__(e); } });
+
+/***/ })
+
+};
+;
